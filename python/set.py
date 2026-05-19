16cd9b948e2c2e7d09f6bd5d1f75d74b550274ad
jayanto561 <jayantoasrker1@gmail.com>
2026-05-19 16:25:38 +0600

    set

---
+++ python/set.py
@@ -0,0 +1,14 @@
+my_set={1, 2, 3, 4, 5}
+print(my_set)
+my_set={1.0,"hello", (1, 2, 3)}
+print(my_set)
+
+
+
+#operations 
+my_set1={1, 2, 3, 4, 5}
+my_set2={4, 5, 6, 7, 8}
+print("Union:", my_set1 | my_set2)
+print("Intersection:", my_set1 & my_set2)
+print("Difference:", my_set1 - my_set2)
+print(sorted(my_set1))
